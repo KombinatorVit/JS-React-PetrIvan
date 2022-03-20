055551119b9c2a950c6c5e83d3fe35b13da7a49e
Kombinator <slonik0775@gmail.com>
2022-03-20 15:36:06 +0200

    my first test commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Test JS');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'Много');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+};
+
+let lastWatchedMovie = prompt('Последний просмотренний фильм', '');
+let filmScore = prompt('Оценка просмотренеего фильма', '');
+let lastWatchedMovie2 = prompt('Последний просмотренний фильм', '');
+let filmScore2 = prompt('Оценка просмотренеего фильма', '');
+
+personalMovieDB.movies[lastWatchedMovie] = [filmScore];
+personalMovieDB.movies[lastWatchedMovie2] = [filmScore2];
+
+
+console.log(personalMovieDB)
