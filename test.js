1b9f58ab53fedd5ba7a61b0e0a816c772a59dbf6
Kombinator <slonik0775@gmail.com>
2022-03-20 22:37:12 +0200

    object Learn

--- test.js
+++ test.js
@@ -1,25 +1,40 @@
-function first() {
-    // Do someting
-setTimeout(function(){
-    console.log(1);
-},500);
-
-}
 
 
-function second(){
-    console.log(2);
-}
 
 
-first();
-second();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log('Test')
+    }
+};
 
-function learnJS(lang,callback){
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// console.log(options.name);
 
-learnJS('JavaScript', function(){
-    console.log('Я прошел этот урок!')
-})
+// delete options.name;
+
+// console.log(options);
+// let counter= 0;
+
+// for ( let key in options){
+//     if(typeof(options[key]) === 'object'){
+//         for(let i in options[key]){
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+// counter++;
+//         }
+//     }else{
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     counter++;
+// }
+// }
+// console.log(counter);
+
+
+
+console.log(Object.keys(options).length);
