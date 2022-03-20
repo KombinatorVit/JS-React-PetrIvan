330ba96d9cd4f30efaddd2e7869c264a9d7000da
Kombinator <slonik0775@gmail.com>
2022-03-20 18:42:05 +0200

    testJsFunction

---
+++ test.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let num = 12;
+
+function showFirstMessage(text) {
+    console.log(text);
+  let num =122222;
+}
+
+showFirstMessage('HELLO WORLD');
+
+
+console.log(num);
+
+function calc(a, b){
+
+    return(a + b);
+}
+
+console.log(calc(2,2));
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotheNum = ret();
+console.log(anotheNum);
