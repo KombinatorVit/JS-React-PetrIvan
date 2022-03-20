30f13dbe4473b360e73f875576edc562403f4720
Kombinator <slonik0775@gmail.com>
2022-03-20 21:49:57 +0200

    callback function learn

--- test.js
+++ test.js
@@ -1,68 +1,25 @@
-// "use strict";
+function first() {
+    // Do someting
+setTimeout(function(){
+    console.log(1);
+},500);
 
-// let num = 12;
-
-// function showFirstMessage(text) {
-//     console.log(text);
-//   let num =122222;
-// }
-
-// showFirstMessage('HELLO WORLD');
+}
 
 
-// console.log(num);
-
-// function calc(a, b){
-
-//     return(a + b);
-// }
-
-// console.log(calc(2,2));
+function second(){
+    console.log(2);
+}
 
 
-// function ret() {
-//     let num = 50;
-//     return num;
-// }
+first();
+second();
 
-// const anotheNum = ret();
-// console.log(anotheNum);
+function learnJS(lang,callback){
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
 
-
-// const test = (a,b) => a+b;
-
-// test(2,333);
-
-// console.log(str[2]);
-
-// const arr = [1,2,3,4,2,2,323,32,4];
-
-// console.log(arr.length);
-
-const str = 'test';
-
-console.log(str.toUpperCase());
-console.log(str);
-console.log(str.toLowerCase());
-console.log(str);
-
-
-let fruit = 'Some fruit';
-
-console.log(fruit.indexOf('g'));
-
-
-const logg = 'Hello world';
-
-// console.log(logg.slice(6,11));
-// console.log(logg.slice(1));
-
-
-// console.log(logg.substring(6,11));
-console.log(logg.substr(6,5));
-
-const num = 12.2;
-console.log(Math.round(num));
-
-const test = '12.2px';
-console.log(parseInt(test));
+learnJS('JavaScript', function(){
+    console.log('Я прошел этот урок!')
+})
