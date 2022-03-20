e80ef92ea558be3a038444e58410c017b7769ba4
Kombinator <slonik0775@gmail.com>
2022-03-20 20:32:42 +0200

    testStringAndNumber

--- test.js
+++ test.js
@@ -1,29 +1,68 @@
-"use strict";
+// "use strict";
 
-let num = 12;
+// let num = 12;
 
-function showFirstMessage(text) {
-    console.log(text);
-  let num =122222;
-}
+// function showFirstMessage(text) {
+//     console.log(text);
+//   let num =122222;
+// }
 
-showFirstMessage('HELLO WORLD');
-
-
-console.log(num);
-
-function calc(a, b){
-
-    return(a + b);
-}
-
-console.log(calc(2,2));
+// showFirstMessage('HELLO WORLD');
 
 
-function ret() {
-    let num = 50;
-    return num;
-}
+// console.log(num);
 
-const anotheNum = ret();
-console.log(anotheNum);
+// function calc(a, b){
+
+//     return(a + b);
+// }
+
+// console.log(calc(2,2));
+
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotheNum = ret();
+// console.log(anotheNum);
+
+
+// const test = (a,b) => a+b;
+
+// test(2,333);
+
+// console.log(str[2]);
+
+// const arr = [1,2,3,4,2,2,323,32,4];
+
+// console.log(arr.length);
+
+const str = 'test';
+
+console.log(str.toUpperCase());
+console.log(str);
+console.log(str.toLowerCase());
+console.log(str);
+
+
+let fruit = 'Some fruit';
+
+console.log(fruit.indexOf('g'))
+
+
+const logg = 'Hello world';
+
+// console.log(logg.slice(6,11));
+// console.log(logg.slice(1));
+
+
+// console.log(logg.substring(6,11));
+console.log(logg.substr(6,5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test))
